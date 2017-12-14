66992f8e23c90fb07b1febd077d1bf144bdf6e5b
Artem Zhukov <zhukovgreen@yandex.ru>
2017-12-14 15:47:55 +0100

    Add nice model reprs

--- kitchen/check_in/models.py
+++ kitchen/check_in/models.py
@@ -4,13 +4,22 @@ from django.db import models
 class Employee(models.Model):
     full_name = models.CharField(max_length=64)
 
+    def __str__(self):
+        return self.full_name
+
 
 class Meal(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField(max_length=512)
+
+    def __str__(self):
+        return f'{self.name} ({self.description[:24]}...)'
 
 
 class Lunch(models.Model):
     employee = models.ForeignKey(to=Employee, on_delete=models.CASCADE)
     meal = models.ForeignKey(to=Meal, on_delete=models.CASCADE)
     date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.employee} ate {self.meal.name} on {self.date}'
